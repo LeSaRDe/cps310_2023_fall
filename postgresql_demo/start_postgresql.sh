804aa3e1a33f1141ade748fe501f4fb7ea26f183
LeSaRDe <urchone@gmail.com>
2023-10-30 19:58:17 -0400

    update start_postgresql.sh

--- postgresql_demo/start_postgresql.sh
+++ postgresql_demo/start_postgresql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "Please specify the data space path: " ds_path
-chown -R $USER:$USER /var/run/postgresql
+sudo chown -R $USER:$USER /var/run/postgresql
 export PGDATA=$ds_path
 pg_ctl start -l $ds_path/logfile 
 
